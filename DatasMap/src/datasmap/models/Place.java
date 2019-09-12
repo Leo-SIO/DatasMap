e0c95224c517fba002501e0794d0d3b15dae0ecc
Leo-SIO <Leo.doubremelle@sts-sio-caen.info>
2019-09-12 09:44:51 +0200

    getters/setters

---
+++ DatasMap/src/datasmap/models/Place.java
@@ -0,0 +1,27 @@
+package datasmap.models;
+
+public class Place {
+	private String address;
+	private Float latitude;
+	private Float longitude;
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Float getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(Float latitude) {
+		this.latitude = latitude;
+	}
+	public Float getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(Float longitude) {
+		this.longitude = longitude;
+	}
+
+}
